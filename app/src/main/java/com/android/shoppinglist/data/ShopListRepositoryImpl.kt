40f8724dcc00198a7fa61a71f8dbd81340161b13
mrMNK <zabuldigaa@gmail.com>
2022-05-25 15:13:37 +0300

    added a simple data layer

---
+++ app/src/main/java/com/android/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.android.shoppinglist.data
+
+import com.android.shoppinglist.domain.ShopItem
+import com.android.shoppinglist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementedId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementedId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+}
