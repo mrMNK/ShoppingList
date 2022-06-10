4af5a66d8bd270f34a38b564e760a29dc98b5ba9
mrMNK <zabuldigaa@gmail.com>
2022-06-10 18:12:14 +0300

    added DB, DB model and DAO

---
+++ app/src/main/java/com/android/shoppinglist/data/AppDatabase.kt
@@ -0,0 +1,36 @@
+package com.android.shoppinglist.data
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun shopListDao(): ShopListDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        private val LOCK = Any()
+        private const val DB_NAME = "shop_item.db"
+
+        fun getInstance(app: Application): AppDatabase {
+            INSTANCE?.let {
+                return it
+            }
+            synchronized(LOCK) {
+                INSTANCE?.let {
+                    return it
+                }
+                val db = Room.databaseBuilder(
+                    app,
+                    AppDatabase::class.java,
+                    DB_NAME
+                ).build()
+                INSTANCE = db
+                return db
+            }
+        }
+    }
+}
--- app/src/main/java/com/android/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/android/shoppinglist/domain/ShopItem.kt
@@ -7,7 +7,6 @@ data class ShopItem(
     var id: Int = UNDEFINED_ID
 ) {
     companion object {
-
         const val UNDEFINED_ID = -1
     }
 }
